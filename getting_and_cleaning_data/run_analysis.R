56de355d9ee8a260091333192e585da6d263651a
Gawaine O'Gilvie <gawaine.ogilvie@hotmail.com>
2017-10-15 13:08:32 -0400

    updated script.

--- getting_and_cleaning_data/run_analysis.R
+++ getting_and_cleaning_data/run_analysis.R
@@ -8,39 +8,64 @@ setwd("./UCI_HAR_Dataset/")
 #---------------------TEST DATA SET---------------------#
 
 feature_names <- read.table("./features.txt", header = FALSE)
-# Test Set
+# LOAD DATA: Test Set
 subjects_ID_num_test <- read.table("./test/subject_test.txt", header = FALSE)
 training_set_test <- read.table("./test/X_test.txt", header = FALSE)
 activity_label_ID_test <- read.table("./test/Y_test.txt", header = FALSE)
-# Train Set
+# LOAD DATA: Train Set
 subjects_ID_num_train <- read.table("./train/subject_train.txt", header = FALSE)
 training_set_train <- read.table("./train/X_train.txt", header = FALSE)
 activity_label_ID_train <- read.table("./train/Y_train.txt", header = FALSE)
 
-# Add column names based on the features list, activity, subjects ID
+# ADD: column names based on the features list, activity, subjects ID
 names(training_set_test) <- feature_names[,2]
 names(training_set_train) <- feature_names[,2]
-names(activity_label_ID_test) <- c("physical_activity(test)")
-names(subjects_ID_num_test) <- c("subject_id(test)")
+names(activity_label_ID_test) <- c("physical_activity_type")
+names(subjects_ID_num_test) <- c("subject_id")
+names(activity_label_ID_train) <- c("physical_activity_type")
+names(subjects_ID_num_train) <- c("subject_id")
 
-# Columns in data frame that are only related to mean and standard deviation, get the index of those columns
-std_col_name_index <- grep("std()", colnames(training_set_test))
-mean_col_name_index <- grep("mean()", colnames(training_set_test))
-# Reduce the number of columns to show only mean() and std()
-only_mean_columns_test <- select(training_set_test, as.vector(mean_col_name_index))
-only_std_columns_test <- select(training_set_test, as.vector(std_col_name_index))
-only_mean_columns_train <- select(training_set_train, as.vector(mean_col_name_index))
-only_mean_columns_train <- select(training_set_train, as.vector(std_col_name_index))
+# FILTER: Columns in data frame that are only related to mean and standard deviation
+std_col_names <- grep("std()", colnames(training_set_test), value = TRUE)
+mean_col_names <- grep("mean()", colnames(training_set_test), value = TRUE)
 
-# Name columns for mean and std data frames
-names(only_mean_columns_test) <- names(training_set_test[mean_col_name_index])
-names(only_std_columns_test) <- names(training_set_test[std_col_name_index])
+# REDUCE: Reduce the number of columns to show only mean() and std() columns
+only_mean_columns_test <- training_set_test[mean_col_name_index]
+only_std_columns_test <- training_set_test[std_col_name_index]
+only_mean_columns_train <- training_set_train[mean_col_name_index]
+only_std_columns_train <- training_set_train[std_col_name_index]
 
-# Combine columns on TEST set, 
-#test_set_narrow <- cbind(only_mean_columns_test, only_std_columns_test, activity_label_ID_test)
+# REMOVE/CLEANUP: Remove original train and test data
+rm(training_set_test, training_set_train)
 
-# Add rows from TRAIN set to combine all the data sets using rbind
+# Combine columns on TEST set and on TRAIN set, and then combine by row 
+test_set_narrow <- cbind(subjects_ID_num_test, activity_label_ID_test, only_mean_columns_test, only_std_columns_test)
+train_set_narrow <- cbind(subjects_ID_num_train, activity_label_ID_train, only_mean_columns_train, only_std_columns_train)
 
-# include other data sets in the Inertia folder
+# Add rows from TRAIN and TEST set to combine all the data sets using rbind
+merged_set <- rbind(test_set_narrow, train_set_narrow)
+
+# FINAL STEP: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+# Update physical activity label colum with real names for the activity
+# test_set_narrow$physical_activity[test_set_narrow$physical_activity == 6,] <- c("LAYING")
+# test_set_narrow$physical_activity[test_set_narrow$physical_activity == 5,] <- c("STANDING")
+# test_set_narrow$physical_activity[test_set_narrow$physical_activity == 4,] <- c("SITTING")
+# test_set_narrow$physical_activity[test_set_narrow$physical_activity == 3,] <- c("WALKING_DOWNSTAIRS")
+# test_set_narrow$physical_activity[test_set_narrow$physical_activity == 2,] <- c("WALKING_UPSTAIRS")
+# test_set_narrow$physical_activity[test_set_narrow$physical_activity == 1,] <- c("WALKING")
+# 
+# train_set_narrow$physical_activity[train_set_narrow$physical_activity == 6,] <- c("LAYING")
+# train_set_narrow$physical_activity[train_set_narrow$physical_activity == 5,] <- c("STANDING")
+# train_set_narrow$physical_activity[train_set_narrow$physical_activity == 4,] <- c("SITTING")
+# train_set_narrow$physical_activity[train_set_narrow$physical_activity == 3,] <- c("WALKING_DOWNSTAIRS")
+# train_set_narrow$physical_activity[train_set_narrow$physical_activity == 2,] <- c("WALKING_UPSTAIRS")
+# train_set_narrow$physical_activity[train_set_narrow$physical_activity == 1,] <- c("WALKING")
+
+
+
+
+# Use GroupBy to average each column by physical activity and subject
 
 #----------------------END------------------------------#
